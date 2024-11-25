725b1ad39f853f4a570cc00600c6415a7e460c2a
muzefa01 <muzefaabbas118@gmail.com>
2024-11-25 14:44:36 +0000

    add get api/topics endpoint

--- models/api.models.js
+++ models/api.models.js
@@ -2,7 +2,6 @@ const db = require("../db/connection");
 
 exports.fetchTopics = () => {
   return db.query("SELECT * FROM topics;").then(({ rows: topics }) => {
-    console.log(topics)
     return topics;
   });
 };
