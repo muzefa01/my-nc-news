584f029f170e733f9543b388de19797b0b145b41
muzefa01 <muzefaabbas118@gmail.com>
2024-11-26 16:25:45 +0000

    add get articles endpoint

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const app = express()
-const {getApi, getTopics, getArticleById} = require("./controllers/api.controller")
+const {getApi, getTopics, getArticleById, getArticles} = require("./controllers/api.controller")
 
 app.get("/api", getApi)
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById)
+app.get("/api/articles", getArticles)
 
 
 app.all("*",(req, res, ) => {
--- models/api.models.js
+++ models/api.models.js
@@ -21,4 +21,25 @@ return Promise.reject({status: 404, msg: "not found" })
   
 })
 }
+exports.fetchArticles = () => {
+  const queryStr = `
+    SELECT 
+      articles.author,
+      articles.title,
+      articles.article_id,
+      articles.topic,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url,
+      COUNT(comments.comment_id) AS comment_count
+    FROM articles
+    LEFT JOIN comments
+    ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC;
+  `;
+    return db.query(queryStr).then(({ rows }) => {
+    return rows; 
+  });
+};
 
