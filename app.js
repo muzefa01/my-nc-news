8a1c447fb6496d3d5d1189ea0cd354d6d19b61dd
muzefa01 <muzefaabbas118@gmail.com>
2024-12-16 17:02:31 +0000

    allow cross origin resource sharing removed specific origin)

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const app = express();
 
 
 const {getApi, getTopics, getArticleById, getArticles, getCommentsByArticleId, postCommentToArticle, patchArticleVotes, deleteComment,getAllUsers} = require("./controllers/api.controller")
-app.use(cors({
-   origin: "http://127.0.0.1:5173"
-}));
+app.use(cors());
 app.use(express.json())
 
 
